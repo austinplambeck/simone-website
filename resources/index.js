c48a8e08bb23288878e36ec7440f9eeaaccc5261
austinplambeck <90013723+austinplambeck@users.noreply.github.com>
2022-03-03 15:38:51 -0800

    Comm7 - version2.0 (mobile)
    also attempted some changes to the JS. trying to make the text appear so it doesn't have to be a fixed/set max-height. not sure how to go about this yet

--- resources/index.js
+++ resources/index.js
@@ -8,11 +8,13 @@ function showMore() {
       article.className = "";
       div.className = "";
       document.querySelector("#show-more-d").innerHTML = "show more";
+      //document.querySelector("#p-test-d").innerHTML = " ";
     } else {
       //read more
       article.className = "open";
       div.className = "open";
       document.querySelector("#show-more-d").innerHTML = "show less";
+      //document.querySelector("#p-test-d").innerHTML = "Curabitur at justo augue. Nam euismod, felis et vestibulum varius, urna eros aliquet nisl, nec blandit massa lorem id mauris. Nulla aliquet risus ante, nec dapibus mauris eleifend vel. Vestibulum eget rutrum enim. Phasellus tincidunt tortor ornare tortor eleifend, et rhoncus sem interdum. Ut pulvinar a mi sed dapibus. Etiam varius lectus lectus, quis laoreet diam venenatis auctor. Nullam ex sapien, suscipit sit amet vehicula nec, mollis at orci.";
     }
  }
 
@@ -20,15 +22,18 @@ function showMore() {
 
 // Mobile
 const articleM = document.querySelector("#info-m");
+const divM = document.querySelector("#container-m");
 
 function showMoreM() {
     if (articleM.className == "open") {
       // read less
       articleM.className = "";
+      divM.className = "";
       document.querySelector("#show-more-m").innerHTML = "show more";
     } else {
       //read more
       articleM.className = "open";
+      divM.className = "open";
       document.querySelector("#show-more-m").innerHTML = "show less";
     }
  }
